2aa9bf775cee5c782b3d011a4d04b5d53bc0b912
laikov <zheka-se@ya.ru>
2021-04-29 00:06:34 +0300

    3/7

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-one, two, three = int(input()), int(input()), int(input())
-mean_score = (one + two + three) / 3
-if mean_score >= 60:
-    print(f'''{mean_score}
-Congratulations, you are accepted!''')
+# one, two, three = int(input()), int(input()), int(input())
+# mean_score = (one + two + three) / 3
+# if mean_score >= 60:
+#     print(f'''{mean_score}
+# Congratulations, you are accepted!''')
+#
+# else:
+#     print(f'''{mean_score}
+# We regret to inform you that we will not be able to offer you admission.''')
 
-else:
-    print(f'''{mean_score}
-We regret to inform you that we will not be able to offer you admission.''')
+
+# Stage 3/7: Going big
+n, m = int(input()), int(input())
+new_arr = []
+for i in range(n):
+    student = input()
+    student = student.split(' ')
+    num = student.pop()
+    new_arr.append([' '.join(student), float(num)])
+
+def take_second(elem):
+    return elem[1]
+sorted_list = sorted(new_arr, key = take_second, reverse=True)
+print('Successful applicants:')
+for i in range(m):
+    print(sorted_list[i][0])
