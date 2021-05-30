835313c028a6565cc60381f69321317c19e3602a
laikov <zheka-se@ya.ru>
2021-05-30 15:36:23 +0300

    laikovski/30-05

--- main.py
+++ main.py
@@ -74,47 +74,188 @@
 
 # Stage 5/7: Special knowledge
 import math
-number_of_students = int(input())
-# open and read file
-f = open('applicants.txt', "rt")
+# number of students that can be accepted into each department
+# N = int(input())
+#
+# departments = {'Biotech': [], 'Chemistry': [], 'Engineering': [], 'Mathematics': [], 'Physics': []}
+# not_sorted_applicants = {}
+#
+# file = open('applicants.txt', encoding='utf-8')
+# not_sorted_applicants = [line.rstrip('\n').split() for line in file]
+# file.close()
+#
+# # applicant input
+# # first name, last name, physics exam, chemistry exam, math exam, cs exam, first priority, second pr, third pr
+#
+# # sorting applicants by exam result, first name, last name
+# physics_applicants = sorted(not_sorted_applicants, key=lambda x: (-int(x[2]), x[0], x[1]))
+# chemistry_applicants = sorted(not_sorted_applicants, key=lambda x: (-int(x[3]), x[0], x[1]))
+# math_applicants = sorted(not_sorted_applicants, key=lambda x: (-int(x[4]), x[0], x[1]))
+# cs_applicants = sorted(not_sorted_applicants, key=lambda x: (-int(x[5]), x[0], x[1]))
+#
+# # copying full list of applicants
+# not_accepted_applicants = physics_applicants[:]
+#
+#
+# # enrolling students into each department by wave
+# def applicants_waves(wave):
+#     global departments
+#     global not_accepted_applicants
+#     for depart, students_list in departments.items():
+#         if depart == 'Biotech' or depart == 'Chemistry':
+#             subject_list = chemistry_applicants
+#         elif depart == 'Engineering':
+#             subject_list = cs_applicants
+#         elif depart == 'Mathematics':
+#             subject_list = math_applicants
+#         else:
+#             subject_list = physics_applicants
+#
+#         for student in subject_list:
+#             if student[wave] == depart:
+#                 if len(students_list) < N:
+#                     students_list.append(student)
+#                     not_accepted_applicants.remove(student)
+#
+#
+# for i in range(6, 9):
+#     applicants_waves(i)
+#
+#     # updating each list after every wave
+#     physics_applicants = sorted(not_accepted_applicants, key=lambda x: (-int(x[2]), x[0], x[1]))
+#     chemistry_applicants = sorted(not_accepted_applicants, key=lambda x: (-int(x[3]), x[0], x[1]))
+#     math_applicants = sorted(not_accepted_applicants, key=lambda x: (-int(x[4]), x[0], x[1]))
+#     cs_applicants = sorted(not_accepted_applicants, key=lambda x: (-int(x[5]), x[0], x[1]))
+#
+#
+# for dep, students in departments.items():
+#     print("\n" + dep)
+#     if dep == 'Biotech':
+#         sorted_students = sorted(students, key=lambda x: (-int(x[3]), x[0], x[1]))
+#         for st in sorted_students:
+#             print(st[0], st[1], st[3])
+#     elif dep == 'Chemistry':
+#         sorted_students = sorted(students, key=lambda x: (-int(x[3]), x[0], x[1]))
+#         for st in sorted_students:
+#             print(st[0], st[1], st[3])
+#     elif dep == 'Engineering':
+#         sorted_students = sorted(students, key=lambda x: (-int(x[5]), x[0], x[1]))
+#         for st in sorted_students:
+#             print(st[0], st[1], st[5])
+#     elif dep == 'Mathematics':
+#         sorted_students = sorted(students, key=lambda x: (-int(x[4]), x[0], x[1]))
+#         for st in sorted_students:
+#             print(st[0], st[1], st[4])
+#     else:
+#         sorted_students = sorted(students, key=lambda x: (-int(x[2]), x[0], x[1]))
+#         for st in sorted_students:
+#             print(st[0], st[1], st[2])
 
-#create dictionary
-dic_student = []
+# Stage 6/7: Extensive training
 
-subjects = {
-    'Biotech': [],
-    'Chemistry': [],
-    'Engineering': [],
-    'Mathematics': [],
-    'Physics': []
-}
-while True:
-    line = f.readline()
-    if not line:
-        break
-    str = line.strip().split(' ')
-    dic_student.append((str[0] + ' ' + str[1], float(math.floor((int(str[2]) + int(str[3]) + int(str[4]) + int(str[5])) / 4)), [str[6], str[7], str[8]]))
+N = int(input())
 
-# sort student
-temp = sorted(dic_student, key=lambda pga: pga[0])
-new = sorted(temp, key=lambda pga: pga[1], reverse=True)
-rolled = []
-for i in range(3):
+departments = {'Biotech': [], 'Chemistry': [], 'Engineering': [], 'Mathematics': [], 'Physics': []}
+not_sorted_applicants = {}
 
-    for student in new:
-        if student[2][i] == ' ':
-            continue
-        elif len(subjects[student[2][i]]) < number_of_students:
-            subjects[student[2][i]].append([student[0], student[1]])
+file = open('applicants.txt', encoding='utf-8')
+biotech_file = open('biotech.txt', 'a', encoding='utf-8')
+chemistry_file = open('chemistry.txt', 'a', encoding='utf-8')
+engineering_file = open('engineering.txt', 'a', encoding='utf-8')
+mathematics_file = open('mathematics.txt', 'a', encoding='utf-8')
+physics_file = open('physics.txt', 'a', encoding='utf-8')
 
-            student[2][0] = ' '
-            student[2][1] = ' '
-            student[2][2] = ' '
+not_sorted_applicants = [line.rstrip('\n').split() for line in file]
+file.close()
 
-for item in subjects:
-    x = sorted(subjects[item], key=lambda x: x[1], reverse=True)
-    # print(x)
-    print(item, len(subjects[item]))
-    for student in x:
-        print(f'{student[0]} {student[1]}')
-    print('')
+# applicant input
+# first name, last name, physics exam, chemistry exam, math exam, cs exam, first priority, second pr, third pr
+
+# sorting applicants by exam result, first name, last name
+physics_applicants = sorted(not_sorted_applicants, key=lambda x: (-int(x[2]), x[0], x[1]))
+chemistry_applicants = sorted(not_sorted_applicants, key=lambda x: (-int(x[3]), x[0], x[1]))
+math_applicants = sorted(not_sorted_applicants, key=lambda x: (-int(x[4]), x[0], x[1]))
+cs_applicants = sorted(not_sorted_applicants, key=lambda x: (-int(x[5]), x[0], x[1]))
+
+# copying full list of applicants
+not_accepted_applicants = physics_applicants[:]
+
+
+# enrolling students into each department by wave
+N = int(input())
+
+departments = {'Biotech': [], 'Chemistry': [], 'Engineering': [], 'Mathematics': [], 'Physics': []}
+not_sorted_applicants = {}
+
+file = open('applicants.txt', encoding='utf-8')
+not_sorted_applicants = [line.rstrip('\n').split() for line in file]
+file.close()
+
+# applicant input
+# first name, last name, physics exam, chemistry exam, math exam, cs exam, first priority, second pr, third pr
+
+# sorting applicants by exam result, first name, last name
+physics_applicants = sorted(not_sorted_applicants, key=lambda x: (-((float(x[2]) + float(x[4])) / 2), x[0], x[1]))
+chemistry_applicants = sorted(not_sorted_applicants, key=lambda x: (-float(x[3]), x[0], x[1]))
+math_applicants = sorted(not_sorted_applicants, key=lambda x: (-float(x[4]), x[0], x[1]))
+cs_applicants = sorted(not_sorted_applicants, key=lambda x: (-((float(x[5]) + float(x[4])) / 2), x[0], x[1]))
+biotech_applicants = sorted(not_sorted_applicants, key=lambda x: (-((float(x[2]) + float(x[3])) / 2), x[0], x[1]))
+
+# applicants = sorted(not_sorted_applicants, key=lambda x: (-float(x[2]), x[0], x[1]))
+not_accepted_applicants = physics_applicants[:]
+
+
+def applicants_waves(wave):
+    global departments
+    global not_accepted_applicants
+    for depart, students_list in departments.items():
+        if depart == 'Biotech':
+            subject_list = biotech_applicants
+        elif depart == 'Chemistry':
+            subject_list = chemistry_applicants
+        elif depart == 'Engineering':
+            subject_list = cs_applicants
+        elif depart == 'Mathematics':
+            subject_list = math_applicants
+        else:
+            subject_list = physics_applicants
+
+        for student in subject_list:
+            if student[wave] == depart:
+                if len(students_list) < N:
+                    students_list.append(student)
+                    not_accepted_applicants.remove(student)
+
+
+for i in range(6, 9):
+    applicants_waves(i)
+    physics_applicants = sorted(not_accepted_applicants, key=lambda x: (-((float(x[2]) + float(x[4])) / 2), x[0], x[1]))
+    chemistry_applicants = sorted(not_accepted_applicants, key=lambda x: (-float(x[3]), x[0], x[1]))
+    math_applicants = sorted(not_accepted_applicants, key=lambda x: (-float(x[4]), x[0], x[1]))
+    cs_applicants = sorted(not_accepted_applicants, key=lambda x: (-((float(x[5]) + float(x[4])) / 2), x[0], x[1]))
+    biotech_applicants = sorted(not_accepted_applicants, key=lambda x: (-((float(x[2]) + float(x[3])) / 2), x[0], x[1]))
+
+
+for dep, students in departments.items():
+    file = open(dep.lower() + '.txt', 'w', encoding='utf-8')
+    if dep == 'Biotech':
+        sorted_students = sorted(students, key=lambda x: (-((float(x[2]) + float(x[3])) / 2), x[0], x[1]))
+        for st in sorted_students:
+            file.write(st[0] + ' ' + st[1] + ' ' + str((float(st[2]) + float(st[3])) / 2) + '\n')
+    elif dep == 'Chemistry':
+        sorted_students = sorted(students, key=lambda x: (-int(x[3]), x[0], x[1]))
+        for st in sorted_students:
+            file.write(st[0] + ' ' + st[1] + ' ' + st[3] + '\n')
+    elif dep == 'Engineering':
+        sorted_students = sorted(students, key=lambda x: (-((float(x[5]) + float(x[4])) / 2), x[0], x[1]))
+        for st in sorted_students:
+            file.write(st[0] + ' ' + st[1] + ' ' + str((float(st[5]) + float(st[4])) / 2) + '\n')
+    elif dep == 'Mathematics':
+        sorted_students = sorted(students, key=lambda x: (-int(x[4]), x[0], x[1]))
+        for st in sorted_students:
+            file.write(st[0] + ' ' + st[1] + ' ' + st[4] + '\n')
+    else:
+        sorted_students = sorted(students, key=lambda x: (-((float(x[2]) + float(x[4])) / 2), x[0], x[1]))
+        for st in sorted_students:
+            file.write(st[0] + ' ' + st[1] + ' ' + str((float(st[2]) + float(st[4])) / 2) + '\n')
+    file.close()
